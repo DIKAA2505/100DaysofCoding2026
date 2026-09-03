8f92054ba89a7c693095dbdc065881cef7094d75
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-03 21:12:28 +0800

    Create Day02.java
    DAY2-DAY100

---
+++ Day02.java
@@ -0,0 +1,14 @@
+class main {
+  public static void main (String[] args){
+    System.out.println("||PERKENALAN DIRI||");
+    System.out.print("namaa sayaa : ");
+    System.out.println("Dika");
+    System.out.printf("umur.   :%dtahun%n",18);
+    System.out.print("Hobii.    : ");
+    System.out.println("Main game");
+    //ayoo Mabar Ml ges
+    /*istirahat*/
+    /**Day1-Day100*/
+    //toloooooooong
+  }
+}
