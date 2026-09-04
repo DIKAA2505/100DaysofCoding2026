d2c83e8ff24ce57f62f8f3319f1b5ac50551cdf6
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-04 13:03:02 +0800

    Day03.java
    DAY3-DAY100

---
+++ Day03.java
@@ -0,0 +1,16 @@
+public class Day3 {
+    public static void main(String[] args) {
+        // 1. \n = Pindah baris baru
+        System.out.println("Nama\n   Dika");
+        // 2. \t = Jarak / tab
+        System.out.println("Umur:\t18 Tahun");
+        // 3. \" = Tanda kutip dua
+        System.out.println("Bilang: \"SAYA AKAN LAWAN!\"");
+        // 4. \' = Tanda kutip satu
+        System.out.println("Kata: 'Mobile legend' itu stres");
+        // 5. \\ = Menulis garis miring \
+        System.out.println("Sikit lagi : Dika\\Imoo\\100");
+        // 6. \b = Hapus satu huruf ke kiri
+        System.out.println("Kompute\br");
+    }
+}
