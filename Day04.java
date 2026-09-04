c3ddff3bbe8ad8995cd8715422504adef3826fce
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-05 00:33:27 +0800

    Day04.java
    Day04-Day100

---
+++ Day04.java
@@ -0,0 +1,15 @@
+class main {
+  public static void main (String []args){
+    byte angkakecil = 100;
+    short angkasedang = 3000;
+    int angkabesar = 15000;
+    long angkabesargila = 80000L;
+    //long untuk angka dan huruf L kayakny 
+    System.out.println("nilai byte :"+angkakecil);
+    System.out.println("nilai short :"+angkasedang);
+    System.out.println("nilai int :"+angkabesar);
+    System.out.println("nilai long :"+angkabesargila);
+    /*kapan day 100*/
+    //kak ajar ka caranya cepat paham coding 
+  }
+}
