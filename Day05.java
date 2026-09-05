c5bdc661e3b4b3121df730e309583c6c46aa0de6
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-06 00:28:02 +0800

    Day05.java
    DAY5-DAY100

---
+++ Day05.java
@@ -0,0 +1,12 @@
+public class main {
+  public static void main (String[] args){
+  float beratku = 50.5F;
+    //float biasa nya dipakai di angka pecahan kecil dan
+    //dan dapat ditambahkan huruf f dibelakangny
+  double tinggiku = 163.5;
+    //double digunakan di angka yg tinggi 
+  System.out.println("berat badanku  :" + beratku+"kg");
+  System.out.println("tinggi badanku :" + tinggiku+"cm");
+  }
+}
+  
