db99ac262b263cb9ca933539ffbe5debd89154ce
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-07 00:16:01 +0800

    Day06.java
    DAY6-DAY100

---
+++ Day06.java
@@ -0,0 +1,17 @@
+class main {
+  public static void main (String [] args){
+  //char digunakan untuk 1 karakter saja 
+  //dan memakai tanda kutip satu
+  char jeniskelamin = 'L';
+  char nilai = 'A';
+  //boolean hanya bisa digunakan untuk true dan false 
+  boolean sudahimooo = true;
+  boolean lulus = true;
+  boolean matilampu = false;
+  System.out.println("jenis kelamin. :"+ jeniskelamin);
+  System.out.println("nilai ujian.   :"+ nilai);
+  System.out.println("Status rank.   :"+ sudahimooo);
+  System.out.println("hasil ujian.   :"+ lulus);
+  System.out.println("Keadaan listrik:"+matilampu);
+  }
+}
