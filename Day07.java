d1c7bd11389f7ada1bc5ede8d38450bfa0140baf
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-07 21:12:21 +0800

    Day07.java
    DAY7-DAY100

---
+++ Day07.java
@@ -0,0 +1,14 @@
+class string{
+  public static void main(String []args){
+  String nama = "Dika";
+  String alamat = "Pasangkayu";
+  String hobi = "Bermain game";
+  String nim = "D02222222";
+  System.out.println("Nama   :" +nama);
+  System.out.println("Alamat :" +alamat);
+  System.out.println("Hobi.  :" +hobi);
+  System.out.println("Nim.   :" +nim);
+  //final string nim = "Doooo";
+  //Sout("Nim. :"+nim);
+  }
+}
