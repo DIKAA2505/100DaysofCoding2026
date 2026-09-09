6cb8400056c918f836baa973be8f1c9045024d84
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-09 23:16:06 +0800

    Day09.java
    DAY9-DAY100

---
+++ Day09.java
@@ -0,0 +1,16 @@
+public class Konstanta {
+  public static void main(String[] args) {
+    
+    //Konstanta: pakai kata final
+    final double USER = 3.478;
+    final int NIM = "D02026";
+    final String PRODI = "Informatika";
+
+    // Tampilkan nilainya
+    System.out.println("USER ke   : " + USER);
+    System.out.println("NIM       : " + NIM);
+    System.out.println("PRODI     : " + PRODI);
+
+    // nim = D0000; → ERROR! Tidak boleh diubah lagi!
+  }
+}
