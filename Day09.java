b6dd51557ed54561058b91e64a7d608bca53112d
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-09 23:20:39 +0800

    Day09.java
    DAY9-DAY100

--- Day09.java
+++ Day09.java
@@ -3,12 +3,13 @@ public class Konstanta {
     
     //Konstanta: pakai kata final
     final double USER = 3.478;
-    final int NIM = "D02026";
+    final int NIM = "002026";
     final String PRODI = "Informatika";
 
     // Tampilkan nilainya
     System.out.println("USER ke   : " + USER);
     System.out.println("NIM       : " + NIM);
+    // klo mau pakai huruf dan angka pakai string
     System.out.println("PRODI     : " + PRODI);
 
     // nim = D0000; → ERROR! Tidak boleh diubah lagi!
