88786581eb1df35ac989ff9f5c4c83945883a808
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-09 12:55:29 +0800

    Day08.java
    DAY8-DAY100

---
+++ Day08.java
@@ -0,0 +1,18 @@
+public class VariabelDasar {
+  public static void main(String[] args) {
+    
+    String nama;
+    int nilai;
+    nama = "Dika";
+    nilai = 75;
+
+    System.out.println("Nama  : " + nama);
+    System.out.println("Nilai : " + nilai);
+
+    nama = "Dika saja"; // ganti nama
+    nilai = 90;            // ganti nilai
+    System.out.println("\nSetelah diubah:");
+    System.out.println("Nama  : " + nama);
+    System.out.println("Nilai : " + nilai);
+  }
+}
