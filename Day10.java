b0ce4a93ef5efa968a3c4e449a7b7004bf53a7fc
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-11 19:32:21 +0800

    Day10.java
    DAY10-DAY100

---
+++ Day10.java
@@ -0,0 +1,20 @@
+class main {
+public static void main(String [] args){
+  //BATAS BYTE
+  System.out.println("batas byte:");
+  System.out.println("Terkecil.  :" + Byte.MIN_VALUE);
+  System.out.println("Terbesar.  :" + Byte.MAX_VALUE);
+  //BATAS SHORT
+  System.out.println("\nBatas short:");
+  System.out.println("Terkecil.  :" + Short.MIN_VALUE);
+  System.out.println("Terbesar.  :" + Short.MAX_VALUE);
+  //BATAS INTEGER 
+  System.out.println("\nBatas Int:");
+  System.out.println("Terkecil.  :" + Integer.MIN_VALUE);
+  System.out.println("Terbesar.  :" + Integer.MAX_VALUE);
+  //BATAS LONG
+  System.out.println("\nBatas long:");
+  System.out.println("Terkecil.  :" + Long.MIN_VALUE);
+  System.out.println("Terbesar.  :" + Long.MAX_VALUE);
+  }
+}
