276287d4cb46b7bfb905aabf4086a2b0035ec604
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-14 20:27:06 +0800

    Create Day13.java
    DAY13-DAY100

---
+++ Day13.java
@@ -0,0 +1,9 @@
+public class apiiiii {
+    public static void main(String[] args) {
+        System.out.printf(
+            "Nama           : Dika%n" +
+            "NIM            : D0225009%n" +
+            "Kelas          : A%n" +
+            "Mahasiswa      : true%n" );
+    }
+}
