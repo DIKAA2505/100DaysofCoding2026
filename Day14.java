2494a108ef74722c889f028cd7d58d68e0557057
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-15 20:49:22 +0800

    Day14.java
    DAY14-DAY100

---
+++ Day14.java
@@ -0,0 +1,10 @@
+class main{
+  public static void main(String []args) {
+  int a = 100;
+  int b = 10;
+  int c = 15;
+  System.out.println("Penjumlahan 100+10 :"+(a+b));
+  System.out.println("pengurangan 100-10 :"+(a-b));
+  System.out.println("100+10-15          :"+(a+b-c));
+  }
+}
