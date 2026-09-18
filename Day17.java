6368572b8412c33cffff12e9f7b60508ed53f30c
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-18 22:45:02 +0800

    Day17.java
    DAY17-DAY100

---
+++ Day17.java
@@ -0,0 +1,16 @@
+class molee {
+  public static void main (String []args){
+  int A = 999; //nilai awal
+  int B = 999; 
+  B -= 333; //pengurangan 
+  int C = 999;
+  C += 333; //pertambahan
+  int D = 999;
+  D /= 333; //pembagian 
+  int E = 999;
+  E *= 333; //perkalian 
+  int F = 999;
+  F %= 333; //modulus
+  System.out.printf("A/t:%d%nB/t:%d%nC/t:%d%nD/t:%d%nE/t:%d%nF/t:%d",A,B,C,D,E,F);
+  }
+} 
