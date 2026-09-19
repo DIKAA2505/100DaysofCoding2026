85de6cba2701634c89c197e7039f7a5aefc983a1
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-19 23:02:18 +0800

    Day18.java
    DAY18-DAY100

--- Day18.java
+++ Day18.java
@@ -1,19 +1,25 @@
-public class {
+public class main {
   public static void main(String[] args) {
-    
-    byte angkaKecil = 100;
-    int angkaBesar = angkaKecil;
-    System.out.println("byte: " + angkaKecil);
-    System.out.println("int : " + angkaBesar);
-
-    int nilai = 7;
-    double nilaiPecah = nilai;
-    System.out.println("\nint   : " + nilai);
-    System.out.println("double: " + nilaiPecah);
-
-    int a = 5;
-    double b = 2.5;
-    double hasil = a + b;
-    System.out.println("\nHasil: " + hasil);
+    byte nilaibyte = 99;
+    short nilaishort = nilaibyte;
+    int nilaiint = nilaishort;
+    long nilailong = nilaiint;
+    float nilaifloat = nilailong;
+    double nilaidouble = nilaifloat;
+    char karakter = 'D'; 
+    int kodekarakter = karakter;
+    boolean status = true;
+    System.out.println("×_×HASIL KONVERSI OTOMATIS×_×");
+    System.out.println("Byte\t:"+nilaibyte);
+    System.out.println("Short\t:"+nilaishort);
+    System.out.println("Int\t:"+nilaiint);
+    System.out.println("Long\t:"+nilailong);
+    System.out.println("Float\t:"+nilaifloat);
+    System.out.println("Double\t:"+nilaidouble);
+    //konversi char ke int
+    System.out.println("Sebelum\t:"+karakter);
+    System.out.println("Sesudah\t:"+kodekarakter);
+    //boolean hanya bernilai true dan false
+    System.out.println("Boolean\t:"+status);
   }
 }
