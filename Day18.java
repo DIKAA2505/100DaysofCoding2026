6529635d3d08c6341345ea79595d9661d54a80e0
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-19 21:37:59 +0800

    Day18.java
    DAY18-DAY100

---
+++ Day18.java
@@ -0,0 +1,19 @@
+public class {
+  public static void main(String[] args) {
+    
+    byte angkaKecil = 100;
+    int angkaBesar = angkaKecil;
+    System.out.println("byte: " + angkaKecil);
+    System.out.println("int : " + angkaBesar);
+
+    int nilai = 7;
+    double nilaiPecah = nilai;
+    System.out.println("\nint   : " + nilai);
+    System.out.println("double: " + nilaiPecah);
+
+    int a = 5;
+    double b = 2.5;
+    double hasil = a + b;
+    System.out.println("\nHasil: " + hasil);
+  }
+}
