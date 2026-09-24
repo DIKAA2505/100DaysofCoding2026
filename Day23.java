f0bf2dd547fa64f8c200ed219c72145faa119c34
DIKAA2505 <dikasmanbbr@gmail.com>
2026-09-24 20:33:35 +0800

    Day23.java
    DAY23-DAY100

---
+++ Day23.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class main {
+  public static void main (String []args){
+  Scanner in = new Scanner (System.in);
+  int sisi1 = in.nextInt ();
+  int sisi2 = in.nextInt ();
+  int luas = sisi1*sisi2 ;
+  System.out.println("Luas persegi\t:"+luas);
+  }
+}
